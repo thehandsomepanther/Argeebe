49bc48c74090ddfc3715dd4f7490c636c686cb86
Joshua Shi <thehandsomepanther@gmail.com>
2015-02-09 00:37:44 -0600

    Added mobile support
    Added makeshift media queries for jQuery, however still needs
    work—changing the screen in the middle of the game messes up the
    animation

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,3 @@
-//calcRGBscore(r_slider.getValue(), newRGB[0], g_slider.getValue(), newRGB[1], b_slider.getValue(), newRGB[2]).toFixed(2).toString().replace('.', '-')
-
 function getRandomColor() {
     var r = Math.floor(Math.random() * 255);
     var g = Math.floor(Math.random() * 255);
@@ -24,7 +22,7 @@ function calcRGBscore(r1, r2, g1, g2, b1, b2) {
 
 function evalScore(score) {
     var eval = "";
-    
+
     if (score < 100) {
         eval = "Come on, seriously?";
     } else if (score < 150) {
@@ -34,11 +32,29 @@ function evalScore(score) {
     } else {
         eval = "Inspect Element is no fun";
     }
-    
+
     return eval;
 }
 
 $(document).ready(function () {
+
+    function resetColors() {
+        r_slider.setValue(255);
+        g_slider.setValue(255);
+        b_slider.setValue(255);
+
+        $('#color-copy-r').html('<b>R 255<b>');
+        $('#color-copy-g').html('<b>G 255<b>');
+        $('#color-copy-b').html('<b>B 255<b>');
+
+        $('#color-r').css('background-color', 'rgb(255, 0, 0);');
+        $('#color-g').css('background-color', 'rgb(0, 255, 0);');
+        $('#color-b').css('background-color', 'rgb(0, 0, 255);');
+
+        $('#random-swatch').css('background-color', 'rgb(' + getRandomColor().toString() + ');');
+        newRGB = getRandomColor();
+        $('.random-copy').html('<b>ARGEEBE</b><br>' + getRandomNumbers() + '<br>Click to Guess');
+    }
 
     var newRGB = getRandomColor();
     var count = 0;
@@ -75,98 +91,78 @@ $(document).ready(function () {
         .on('slide', changeB)
         .data('slider');
 
+
+
     $('.nametag-random').click(function () {
 
-        if (count % 2 == 0) {
-            
-            var score = calcRGBscore(r_slider.getValue(), newRGB[0], g_slider.getValue(), newRGB[1], b_slider.getValue(), newRGB[2]);
-            
-            $('#instructions').html(' ');
-            $('.col-xs-1').fadeOut(500);
-            $('#guess-swatch').css('background-color', 'rgb(' + r_slider.getValue() + ', ' + g_slider.getValue() + ', ' + b_slider.getValue() + ');');
-            $('#guess-swatch').show();
-            $('#guess-swatch').fadeIn();
+        var score = calcRGBscore(r_slider.getValue(), newRGB[0], g_slider.getValue(), newRGB[1], b_slider.getValue(), newRGB[2]);
 
-            if (r_slider.getValue() + g_slider.getValue() + b_slider.getValue() == 0) {
-                $('#guess-swatch').css('border-color', 'black');
-                $('#guess-swatch').css('border-style', 'solid');
-                $('#guess-swatch').css('border-width', '1px');
+        if ($('.swatch-color').css('left') == '50px') {
+            if (count % 2 == 0) {
+                $('#guess-swatch').css('background-color', 'rgb(' + r_slider.getValue() + ', ' + g_slider.getValue() + ', ' + b_slider.getValue() + ');');
+                $('#guess-swatch').show();
+                $('#guess-swatch').fadeIn();
+                $('#guess-swatch').animate({
+                    top: '257px'
+                }, {
+                    duration: 500
+                });
+
+                $('.guess-copy').html('<b>YOUR GUESS</b><br>R' + r_slider.getValue() + ' G' + g_slider.getValue() + ' B' + b_slider.getValue() + '<br>' + evalScore(score));
+                $('.random-copy').html('<b>ARGEEBE</b><br>R' + newRGB[0] + ' G' + newRGB[1] + ' B' + newRGB[2] + '<br>Click to Play Again');
+
+                count++;
             } else {
-                $('#guess-swatch').css('border-style', 'none');
+                resetColors();
+                $('#guess-swatch').hide();
+                count++;
             }
-            $('#random-swatch').animate({
-                right: '300px'
-            }, {
-                duration: 500,
-                queue: false
-            });
-            $('#guess-swatch').animate({
-                left: '300px'
-            }, {
-                duration: 500,
-                queue: false
-            });
-            $('.guess-copy').html('<b>YOUR GUESS</b><br>R' + r_slider.getValue() + ' G' + g_slider.getValue() + ' B' + b_slider.getValue() + '<br>' + evalScore(score));
-            $('.random-copy').html('<b>ARGEEBE</b><br>R' + newRGB[0] + ' G' + newRGB[1] + ' B' + newRGB[2] + '<br>Click to Play Again');
-            count++;
         } else {
-            $('#instructions').html('                                <h3>Instructions<br> <small>1. Take a look at the color given to you in the swatch. <br>2. Input the red, green, and blue values you think make up the color. <br>3. Click on the swatch to see how you did. Scores range from 0 to 255, with 255 being the best.</small></h3>');
-            $('#random-swatch').animate({
-                right: '0px'
-            }, {
-                duration: 500,
-                queue: false
-            });
-            $('.col-xs-1').fadeIn(500);
-            $('#guess-swatch').hide();
-            $('#guess-swatch').css('left', '0');
+            if (count % 2 == 0) {
 
-            r_slider.setValue(255);
-            g_slider.setValue(255);
-            b_slider.setValue(255);
+                $('#instructions').html(' ');
+                $('.col-xs-1').fadeOut(500);
+                $('#guess-swatch').css('background-color', 'rgb(' + r_slider.getValue() + ', ' + g_slider.getValue() + ', ' + b_slider.getValue() + ');');
+                $('#guess-swatch').show();
+                $('#guess-swatch').fadeIn();
 
-            $('#color-copy-r').html('<b>R 255<b>');
-            $('#color-copy-g').html('<b>G 255<b>');
-            $('#color-copy-b').html('<b>B 255<b>');
+                if (r_slider.getValue() + g_slider.getValue() + b_slider.getValue() == 0) {
+                    $('#guess-swatch').css('border-color', 'black');
+                    $('#guess-swatch').css('border-style', 'solid');
+                    $('#guess-swatch').css('border-width', '1px');
+                } else {
+                    $('#guess-swatch').css('border-style', 'none');
+                }
+                $('#random-swatch').animate({
+                    right: '20vw'
+                }, {
+                    duration: 500,
+                    queue: false
+                });
+                $('#guess-swatch').animate({
+                    left: '20vw'
+                }, {
+                    duration: 500,
+                    queue: false
+                });
+                $('.guess-copy').html('<b>YOUR GUESS</b><br>R' + r_slider.getValue() + ' G' + g_slider.getValue() + ' B' + b_slider.getValue() + '<br>' + evalScore(score));
+                $('.random-copy').html('<b>ARGEEBE</b><br>R' + newRGB[0] + ' G' + newRGB[1] + ' B' + newRGB[2] + '<br>Click to Play Again');
+                count++;
+            } else {
+                $('#instructions').html('                                <h3>Instructions<br> <small>1. Take a look at the color given to you in the swatch. <br>2. Input the red, green, and blue values you think make up the color. <br>3. Click on the swatch to see how you did. Scores range from 0 to 255, with 255 being the best.</small></h3>');
+                $('#random-swatch').animate({
+                    right: '0px'
+                }, {
+                    duration: 500,
+                    queue: false
+                });
+                $('.col-xs-1').fadeIn(500);
+                $('#guess-swatch').hide();
+                $('#guess-swatch').css('left', '0');
 
-            $('#color-r').css('background-color', 'rgb(255, 0, 0);');
-            $('#color-g').css('background-color', 'rgb(0, 255, 0);');
-            $('#color-b').css('background-color', 'rgb(0, 0, 255);');
-
-            $('#random-swatch').css('background-color', 'rgb(' + getRandomColor().toString() + ');');
-            newRGB = getRandomColor();
-            $('.random-copy').html('<b>ARGEEBE</b><br>' + getRandomNumbers() + '<br>Click to Guess');
-
-            count++;
+                resetColors();
+                count++;
+            }
         }
     });
-
-    //    $('.nametag-guess').click(function () {
-    //        $('#instructions').html('                                <h3>Instructions<br> <small>1. Take a look at the color given to you in the swatch. <br>2. Input the red, green, and blue values you think make up the color. <br>3. Click on the swatch to see how you did. Scores range from 0 to 255, with 255 being the best.</small></h3>');
-    //        $('#random-swatch').animate({
-    //            right: '0px'
-    //        }, {
-    //            duration: 500,
-    //            queue: false
-    //        });
-    //        $('.col-xs-1').fadeIn(500);
-    //        $('#guess-swatch').hide();
-    //        $('#guess-swatch').css('left', '0');
-    //
-    //        r_slider.setValue(255);
-    //        g_slider.setValue(255);
-    //        b_slider.setValue(255);
-    //
-    //        $('#color-copy-r').html('<b>R 255<b>');
-    //        $('#color-copy-g').html('<b>G 255<b>');
-    //        $('#color-copy-b').html('<b>B 255<b>');
-    //
-    //        $('#color-r').css('background-color', 'rgb(255, 0, 0);');
-    //        $('#color-g').css('background-color', 'rgb(0, 255, 0);');
-    //        $('#color-b').css('background-color', 'rgb(0, 0, 255);');
-    //
-    //        $('#random-swatch').css('background-color', 'rgb(' + getRandomColor().toString() + ');');
-    //        newRGB = getRandomColor();
-    //    });
-
 });
